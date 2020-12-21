589ec92b3a8a006ec8f173a6122ef9c351a7ea9c
Alexis Montilla <alexisamm9261@gmail.com>
2020-12-21 17:52:14 -0400

    a

--- conexion.php
+++ conexion.php
@@ -1,3 +1,3 @@
 <?php
-$conn = new mysqli("localhost","adminoxas","ishdkm2n29kz","oxa");
+$conn = new mysqli("localhost","adminoxas","Adminoxas1.","oxa");
 if ($conn->connect_error){die("Connection failed: ".$conn->connect_error);}?>
