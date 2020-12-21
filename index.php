afd0e4e5f9506f34db2a7e44d2ca861cd49b5aea
Alexis Montilla <alexisamm9261@gmail.com>
2020-12-21 18:10:39 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require '/administracion/common/conexion.php';
-require "/administracion/precios/index.php";
+require 'app.oxas.tech/administracion/common/conexion.php';
+require "app.oxas.tech/administracion/precios/index.php";
 if(isset($_GET['r'])){$respuesta_form=$_GET['r'];}
 ?>
 ﻿<!DOCTYPE html>
